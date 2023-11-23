b8ba59a63549ffeffe21c9a7ac8061529af6be84
Daffodils&Chocolates <101703530+Daffodils-and-Chocolates@users.noreply.github.com>
2023-11-23 09:50:03 +0000

    after 1st day

---
+++ src/book_frontend/src/data.js
@@ -0,0 +1,42 @@
+import React from 'react';
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        id:1,
+        questions : "swadha's last name is _____",
+        options :[
+           'sharma',
+           'featherington' ,
+           'sareen'
+        ]
+    },
+    {
+        id:2,
+        questions : "Shreya loves hitting who _____",
+        options :[
+           'achintya',
+           'simran' ,
+           'her brother'
+        ]
+    },
+    {
+    id:3,
+    questions : "what is the incorrect answer in this aesthetic _____",
+    options :[
+       'salt air',
+       'red lip' ,
+       'rust on the door'
+    ]
+},
+{
+id:4,
+questions : "select pairs that DO NOT go together _____",
+options :[
+   'kate - anthony',
+   'taylor swift - travis kelce' ,
+   'taylor swift - joe alywn'
+]
+},
+]
+
+export const answers = [0,0,1,1];
